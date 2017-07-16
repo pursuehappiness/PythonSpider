a505de3b6f14e5ca0478aeacecadf517f3dea080
Lucient Lau <pursueexcel@gmail.com>
2017-07-16 22:42:31 +0800

    commit the spider code do not test

--- spider/SGMLParser_demo.py
+++ spider/SGMLParser_demo.py
@@ -1,28 +1,8 @@
-import urllib.request
-#from sgmllib import SGMLParser
-#
-#class ListName(SGMLParser):
-#
-#	def __init__(self):
-#		SGMLParser.__init__(self)
-#		self.is_h4 = ""  
-#		self.name = []
-#	def start_h4(self, attrs):
-#		self.is_h4 = 1
-#	def end_h4(self):  
-#		self.is_h4 = ""  
-#	def handle_data(self, text):  
-#		if self.is_h4 == 1:  
-#			self.name.append(text)  
-
-if __name__ == '__main__':
-    response = urllib.request.urlopen('http://www.baidu.com')  
-    content = response.read()
-    print(content.decode('gbk','ignore').encode('utf-8'))
-    #listname = ListName()  
-    #listname.feed(content)  
-    #for item in listname.name:  
-    #    print(item.decode('gbk').encode('utf8'))
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit([0,0],[1,1],[2,2],[0,1,2])
+LinearRegression(copy_X = True, fit_intercept=True,n_jobs = 1, normalize=False)
+print(reg.coef_)
 
 
 
