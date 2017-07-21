55b52666abe577614fe8de176bf08d9fcb40a7c5
Lucient Lau <pursueexcel@gmail.com>
2017-07-21 21:47:24 +0800

    Modify some code

--- AWebCrawlerWithAsyncioCoroutines/AsyncioCrawler/fetchurl.py
+++ AWebCrawlerWithAsyncioCoroutines/AsyncioCrawler/fetchurl.py
@@ -4,18 +4,60 @@ from selectors import DefaultSelector,EVENT_WRITE
 selector = DefaultSelector()
 
 def fetch(url):
-	sock = socket.socket()
-	sock.setblocking(False)
-	try:
-		socket.connect(('xkcd.com',80))
-	except BlockingIOError:
-		pass
+	fetcher = Fetcher('/353/')
+	fetcher.fetch()
+
+	while True:
+		events = selector.select()
+
+		for event_key,event_mask in events:
+			callback = event_key.data
+			callback(event_key,event_mask)
 
 def connected():
 	selector.unregister(sock.fileno())
 	print('connected')
 
+def loop():
+	while  True:
+		events = selector.select()
+		for event_key, event_mask in events :
+			callback = event_key.data
+			callback()
+
 selector.register(sock.fileno(),EVENT_WRITE,connected)
+
+urls_todo = set(['/'])
+seen_urls = set(['/'])
+
+class Fetcher:
+	def __init__(self,url):
+		self.response = b''
+		self.url = url
+		self.sock = None
+
+	def fetch(self):
+		self.sock = socket.socket()
+		self.sock.setblocking(False)
+		try:
+			self.sock.connect(('xkcd.com',80))
+		except BlockingIOError:
+			pass
+
+		selector.register(self.sock.fileno(),
+			EVENT_WRITE,self.connected
+			)
+
+	def connected(self,key,mask):
+		print('connected')
+
+		selector.unregister(key.fd)
+		request = 'Get {} HTTP/1.0\r\nHost:xkcd.com\r\n\r\n'.format(self.url)
+		self.sock.send(request.encode('ascii'))
+
+		selector.register(key.fd,EVENT_READ,self.read_response)
+
+
 
 if __name__ == '__main__':
 	fetch()
