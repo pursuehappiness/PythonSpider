3870f5df1a2ccc086d9fe72deb46fb01b815b4b1
Lucient Lau <pursueexcel@gmail.com>
2017-07-21 22:28:48 +0800

    fetching url by using thread and queue

--- AWebCrawlerWithAsyncioCoroutines/AsyncioCrawler/fetchurl.py
+++ AWebCrawlerWithAsyncioCoroutines/AsyncioCrawler/fetchurl.py
@@ -1,63 +1,121 @@
+#new start
 
-from selectors import DefaultSelector,EVENT_WRITE
+#using thread
 
-selector = DefaultSelector()
+from queue import Queue 
+from threading import Lock,Thread 
+import urllib.parse
+import socket
+import re
+import time
 
-def fetch(url):
-	fetcher = Fetcher('/353/')
-	fetcher.fetch()
-
-	while True:
-		events = selector.select()
-
-		for event_key,event_mask in events:
-			callback = event_key.data
-			callback(event_key,event_mask)
-
-def connected():
-	selector.unregister(sock.fileno())
-	print('connected')
-
-def loop():
-	while  True:
-		events = selector.select()
-		for event_key, event_mask in events :
-			callback = event_key.data
-			callback()
-
-selector.register(sock.fileno(),EVENT_WRITE,connected)
-
-urls_todo = set(['/'])
 seen_urls = set(['/'])
+lock = Lock()
 
-class Fetcher:
-	def __init__(self,url):
-		self.response = b''
-		self.url = url
-		self.sock = None
+class Fethcher(Thread):
+	def __init__(self,tasks):
+		Thread.__init__(self)
+		self.tasks = tasks
+		self.deamon = True
 
-	def fetch(self):
-		self.sock = socket.socket()
-		self.sock.setblocking(False)
-		try:
-			self.sock.connect(('xkcd.com',80))
-		except BlockingIOError:
-			pass
+		self.start()
 
-		selector.register(self.sock.fileno(),
-			EVENT_WRITE,self.connected
-			)
+	def run(self):
+		while True:
+			url = self.tasks.get()
+			print(url)
+			sock = socket.socket()
+			sock.connect(('localhost',3000))
+			get = 'Get {} HTTP/1.0\r\nHost: localhost\r\n\r\n'.format(url)
+			sock.send(get.encode('ascii'))
+			response = b''
+			chunk = sock.recv(4096)
 
-	def connected(self,key,mask):
-		print('connected')
-
-		selector.unregister(key.fd)
-		request = 'Get {} HTTP/1.0\r\nHost:xkcd.com\r\n\r\n'.format(self.url)
-		self.sock.send(request.encode('ascii'))
-
-		selector.register(key.fd,EVENT_READ,self.read_response)
+			while chunk:
+				response += chunk
+				chunk = sock.recv(4096)
 
 
+			links = self.parse_links(url,response)
+
+			lock.acquire()
+			for link in links.difference(seen_urls):
+				self.tasks.put(link)
+			seen_urls.update(links)
+			lock.release()
+
+			self.tasks.task_done()
+
+	def parse_links(self,fetched_url,response):
+		if not response:
+			print('error: {}'.format(fetched_url))
+			return set()
+
+		if not self._is_html(response):
+			return set()
+
+		urls = set(re.findall(r'''(?i)href=["']?([^\s"'<>]+)''',
+			self.body(response)))
+
+		links = set()
+
+		for url in urls:
+			normalized = urllib.parse.urljoin(fetched_url,url)
+			parts = urllib.parse.urlparse(normalized)
+			if parts.scheme not in ('','http','https'):
+				continue
+			host,port = urllib.parse.splitport(parts.netloc)
+			if host and host.lower() not in ('localhost'):
+				continue
+			defragmented,frag = urllib.parse.urldefrag(parts.path)
+			links.add(defragmented)
+
+		return links
+
+	def body(self,response):
+		body = response.split(b'\r\n\r\n',1)[1]
+		return body.decode('utf-8')
+
+	def _is_html(self,response):
+		head,body = response.split(b'\r\n\r\n',1)
+		headers = dict(h.split(': ') for h in head.decode().split('\r\n')[1,:])
+		return headers.get('Content-Type','').startswith('text/html')
+
+class ThreadPool:
+	"""docstring for ThreadPool"""
+	def __init__(self, num_thread):
+		self.tasks = Queue()
+		for _ in range(num_thread):
+			Fethcher(self.tasks)
+
+	def add_task(self,url):
+		self.tasks.put(url)
+
+	def wait_completion(self):
+		self.tasks.join()
 
 if __name__ == '__main__':
-	fetch()
+	#pass
+
+	start = time.time()
+	print('start time',start)
+	pool = ThreadPool(4)
+	print('created thread pool')
+	pool.add_task("/")
+	print('add task')
+	pool.wait_completion()
+	print('thread complete')
+	print('{} URLs fetched in {:.1f} seconds'.format(len(seen_urls),time.time() - start))
+			
+
+
+
+
+
+
+
+
+
+
+
+
