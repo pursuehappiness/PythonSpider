4b6b30c23e111f2ede0f34cd44b9d6b156cdb01b
Lucient Lau <pursueexcel@gmail.com>
2017-07-23 23:25:48 +0800

    add selector finish test it run correct

--- AWebCrawlerWithAsyncioCoroutines/AsyncioCrawler/SFetcher.py
+++ AWebCrawlerWithAsyncioCoroutines/AsyncioCrawler/SFetcher.py
@@ -1,4 +1,7 @@
-from selectors import DefaultSelector,EVENT_WRITE
+from selectors import *
+import socket
+import urllib.parse
+import time
 
 urls_todo = set(['/'])
 seen_urls = set(['/'])
@@ -25,6 +28,74 @@ class Fetcher:
 		except BlockingIOError:
 			pass
 		selector.register(self.sock.fileno(),EVENT_WRITE,self.connected)
+
+	def connected(self,key,mask):
+		selector.unregister(key.fd)
+		get = "GET {} HTTP/1.0\r\nHost: localhost\r\n\r\n".format(self.url)
+		self.sock.send(get.encode('ascii'))
+		selector.register(key.fd,EVENT_READ,self.read_response)
+
+	def read_response(self,key,mask):
+		global stopped
+
+		chunk = self.sock.recv(4096) #chunk size
+		if chunk:
+			self.response += chunk
+		else:
+			selector.unregister(key.fd) #Done readding
+			links = self.parse_links()
+			for link in links.difference(seen_urls):
+				urls_todo.add(link)
+				Fetcher(link).fetch()
+
+			seen_urls.update(links)
+			urls_todo.remove(self.url)
+			if not urls_todo:
+				stopped = True
+			print(self.url)
+
+	def body(self):
+		body = self.response.split(b'\r\n\r\n',1)[1]
+		return body.decode('utf-8')
+
+	def parse_links(self):
+		if not self.response:
+			print('error: {}'.format(self.url))
+			return set()
+		if not self._is_html():
+			return set()
+		urls = set(re.findall(r'''(?i)href=["']?([^\s"'<>]+)''',self.body()))
+
+		links = set()
+		for url in urls:
+			normalized = urllib.parse.urljoin(self.url,url)
+			parts = urllib.parse.urlparse(normalized)
+			if parts.scheme not in ('','http','https'):
+				continue
+			host,port = urllib.parse.splitport(parts.netloc)
+			if host and host.lower() not in ('localhost'):
+				continue
+			defragmented,frag = urllib.parse.urldefrag(parts.path)
+			link.add(defragmented)
+
+		return links 
+
+	def _is_html(self):
+		head, body = self.response.split(b'\r\n\r\n',1)
+		headers = dict(h.split(': ') for h in head.decode().split('\r\n')[1:])
+		return headers.get('Content-Type','').startswith('text/html')
+
+start = time.time()
+fetcher = Fetcher('/')
+fetcher.fetch()
+while not stopped:
+    events = selector.select()
+    for event_key, event_mask in events:
+        callback = event_key.data
+        callback(event_key, event_mask)
+
+print('{} URLs fetched in {:.1f} seconds, achieved concurrency = {}'.format(
+    len(seen_urls), time.time() - start, concurrency_achieved))
 
 
 
